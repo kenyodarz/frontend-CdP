3bad9f385cb6e6151b82dd7056e24867d9b02ceb
Jorge Mario Mina Diaz <jmina@bancolombia.com.co>
2025-12-05 19:03:23 -0500

    feat: Add new inventory document listing component.

---
+++ src/app/features/inventario/listar-documentos/listar-documentos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-documentos',
+  imports: [],
+  templateUrl: './listar-documentos.html',
+  styleUrl: './listar-documentos.scss',
+})
+export class ListarDocumentos {
+
+}
